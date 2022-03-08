cd261dbafb744c7b45d3d422ee326baff5e7bfa9
Rohianon <41194018+Rohianon@users.noreply.github.com>
2022-03-09 00:34:09 +0300

    Update jumia.py

--- jumia.py
+++ jumia.py
@@ -12,7 +12,7 @@ def getstatuscode(URL):
     return response.status_code
 
 
-def getsoup(URL):
+def get_pagecontent(URL):
     if getstatuscode(URL) == 200:
         response = requests.get(URL)
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -31,8 +31,8 @@ def getproductname(soup):
         print("No product name was scraped")
         return None
     
-    def getprice(soup):
-        '''Get the prie of the product'''
+def getproductprice(soup):    
+    '''Get the prie of the product'''
     price_lst = re.findall('KSH\$([0-9.,]+)', str(soup.find_all('span', {'class':'prc'})))
 
     if len(price_lst) > 0:
@@ -53,3 +53,33 @@ def getproductname(soup):
         return [name, price, URL]
     except:
         print('There was a problem scraping the product on this link: ', URL)
+        
+
+# calling out the functions and creating a
+## list of list containing the data
+soup = get_pagecontent(url)
+name = getproductname(soup)
+brand = getproductbrand(soup)
+price = getproductprice(soup)
+discount = getproductdiscount(soup)
+review = getproductreviewcnt(soup)
+rating = getproductrating(soup)
+
+list_of_lists = [name, brand, price, discount, review,rating]
+
+## Save and review the product data
+with open('jumia_products.csv', 'w') as jumia_file:
+    fieldnames = ["name", "brand", "price", "discount", "reviews", "rating"]
+    
+    csvwriter = csv.write(jumia_file)
+    csvwriter.write(fieldnames)
+    
+    #loop through product list to update csv file
+    for product in list_of_lists:
+        csvwriter.write(product)
+        
+    print("Done! All products have been added to CSV file")
+
+## Using pandas to do data manipulation
+jumia = pd.read_csv('jumia_products.csv')
+jumia.head(10)        
